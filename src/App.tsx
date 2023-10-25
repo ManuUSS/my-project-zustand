b00a850d4fe1fc5fdcd0e07f762998458212ac36
Manuel Ulate <fr3zs09@gmail.com>
2023-10-24 20:59:02 -0600

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-center font-bold underline">
+        Hello world!!!
+      </h1>
+    </>
+  )
+}
+
+export default App
