e3acd7f75eb99d80139ff62a2e117983a3609abe
Manuel Ulate <fr3zs09@gmail.com>
2023-10-25 11:22:30 -0600

    fix: main list page

--- src/modules/characters/components/CharacterCard.tsx
+++ src/modules/characters/components/CharacterCard.tsx
@@ -1,7 +1,25 @@
 
 
+/**
+ * 
+ * @returns 
+ */
 export const CharacterCard = () => {
   return (
-    <article className="bg-sky-300">01</article>
+    <article className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <div>
+            <img 
+                className="rounded-t-lg" 
+                src="https://www.geekmi.news/__export/1691600122329/sites/debate/img/2023/08/09/geekmi_-_2023-08-09t095403_909.jpg_554688468.jpg" 
+                alt="Satoru Gojo & Geto" 
+            />
+        </div>
+        <div className="p-5">
+            <div className="flex justify-between">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Saturo Gojo</h5>
+                
+            </div>
+        </div>
+    </article>
   )
 }
---
+++ src/modules/characters/components/index.ts
@@ -0,0 +1,2 @@
+
+export { CharacterCard } from "./CharacterCard";
