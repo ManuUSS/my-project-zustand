b3e87d9fd69b0b61e39e4b41f71d6ee8f2c49125
Manuel Ulate <fr3zs09@gmail.com>
2023-10-24 21:27:54 -0600

    new: initial aside

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Sidebar } from "./modules/shared/components"
 
 function App() {
 
   return (
     <>
+      <Sidebar />
       <h1 className="text-3xl text-center font-bold underline">
         Hello world!!!
       </h1>
---
+++ src/modules/shared/components/Sidebar.tsx
@@ -0,0 +1,61 @@
+import { PlusIcon, HeartIcon, HandThumbDownIcon, HandThumbUpIcon, UsersIcon } from "@heroicons/react/24/outline";
+
+/**
+ * 
+ * @returns 
+ */
+export const Sidebar = () => {
+    return (
+        <aside id="sidebar" 
+            className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar"
+        >   
+        <div>
+
+        </div>
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <ul className="space-y-2 font-medium">
+                <li>
+                    <button 
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full text-start"
+                    >
+                        <UsersIcon className="w-5" />
+                        <span className="ml-3">Lista de personajes</span>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
+                    >
+                        <HandThumbUpIcon className="w-5" />
+                        <span className="ml-3">Personajes vivos</span>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
+                    >
+                        <HandThumbDownIcon className="w-5" />
+                        <span className="ml-3">Personajes muertos</span>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
+                    >
+                        <HeartIcon className="w-5" />
+                        <span className="ml-3">Mis favoritos</span>
+                    </button>
+                </li>
+                <li>
+                    <button 
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
+                    >
+                        <PlusIcon className="w-5"/>
+                        <span className="ml-3">Agregar nuevo</span>
+                    </button>
+                </li>
+            </ul>
+        </div>
+        </aside>
+    )
+}
