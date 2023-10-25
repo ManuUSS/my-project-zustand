b6e27e7671daf211eb173c72c4bcf02087c3fc29
Manuel Ulate <fr3zs09@gmail.com>
2023-10-24 22:20:03 -0600

    new: initial layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,9 @@ export const MainLayout = ( { children }: PropsWithChildren ) => {
   return (
     <body>
         <Sidebar />
-            <main>
+            <main
+                className="float-right inline w-[calc(100%-16rem)] h-[calc(100%-77.53px)]"
+            >
                 { children }
             </main>
         <Footer />
