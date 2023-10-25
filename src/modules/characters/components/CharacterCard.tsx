54eaa984e15b8b68cbb799f4e3e9852895c7a37f
Manuel Ulate <fr3zs09@gmail.com>
2023-10-25 12:45:39 -0600

    fix: types on char card

--- src/modules/characters/components/CharacterCard.tsx
+++ src/modules/characters/components/CharacterCard.tsx
@@ -19,54 +19,55 @@ interface Props {
  */
 export const CharacterCard:FC<Props> = ({ character }) => {
 
-  const validateStatus = ( status: Status ) => {
-    if( status === "alive" )
-        return "text-green-500"
-    else if ( status === "dead" )
-        return "text-red-500"
-    
-    return "text-slay-500"
-}
+    const validateStatus = ( status: Status ) => {
+        if( status === "alive" )
+            return "text-green-500"
 
-  return (
-    <article className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-        <div>
-            <img 
-                className="rounded-t-lg" 
-                src={ character.image } 
-                alt={ character.name } 
-            />
-        </div>
-        <div className="p-5">
-            <div className="flex justify-between items-center mb-2">
-                <div>
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900">{ character.name }</h5>
-                    <p className="">{ character.serie }</p>
-                </div>
-                <StopIcon 
-                    className={`w-5 ${ validateStatus( character.status )}`} 
+        else if ( status === "dead" )
+            return "text-red-500"
+
+        return "text-slate-400"
+    }
+
+    return (
+        <article className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+            <div>
+                <img 
+                    className="rounded-t-lg" 
+                    src={ character.image } 
+                    alt={ character.name } 
                 />
             </div>
-            <p className="mb-3 font-normal text-gray-700">
-                { textProof.slice(0, 150) + "..."}
-            </p>
-            <div
-                className="flex justify-between items-center"
-            >
-                <button
-                    type="button"
-                    className="text-white bg-sky-500 hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            <div className="p-5">
+                <div className="flex justify-between items-center mb-2">
+                    <div>
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900">{ character.name }</h5>
+                        <p className="">{ character.serie }</p>
+                    </div>
+                    <StopIcon 
+                        className={`w-5 ${ validateStatus( character.status )}`} 
+                    />
+                </div>
+                <p className="mb-3 font-normal text-gray-700">
+                    { textProof.slice(0, 150) + "..."}
+                </p>
+                <div
+                    className="flex justify-between items-center"
                 >
-                    Guardar como favorito
-                </button>
-                <button
-                    type="button"
-                    className="text-gray-600 hover:text-white border border-gray-500 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center"
-                >
-                    <EyeIcon width={ 12 } />
-                </button>
+                    <button
+                        type="button"
+                        className="text-white bg-sky-500 hover:bg-sky-600 focus:ring-4 focus:outline-none focus:ring-sky-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    >
+                        Guardar como favorito
+                    </button>
+                    <button
+                        type="button"
+                        className="text-gray-600 hover:text-white border border-gray-500 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-2.5 text-center"
+                    >
+                        <EyeIcon width={ 12 } />
+                    </button>
+                </div>
             </div>
-        </div>
-    </article>
-  )
+        </article>
+    )
 }
