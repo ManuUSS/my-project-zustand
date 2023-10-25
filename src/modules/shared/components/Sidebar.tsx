14d94fe90482067005f1f07e17ac17f5991309d0
Manuel Ulate <fr3zs09@gmail.com>
2023-10-24 21:48:38 -0600

    fix: initial aside

--- src/modules/shared/components/Sidebar.tsx
+++ src/modules/shared/components/Sidebar.tsx
@@ -7,13 +7,14 @@ import { PlusIcon, HeartIcon, HandThumbDownIcon, HandThumbUpIcon, UsersIcon } fr
 export const Sidebar = () => {
     return (
         <aside id="sidebar" 
-            className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar"
+            className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" 
+            aria-label="Sidebar"
         >   
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium">
+            <ul className="space-y-2 text-lg">
                 <li>
                     <button 
-                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full text-start"
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 w-full"
                     >
                         <UsersIcon className="w-5 text-teal-500" />
                         <span className="ml-3">Lista de personajes</span>
