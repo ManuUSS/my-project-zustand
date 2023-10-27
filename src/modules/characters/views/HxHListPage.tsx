c044c67e7a935207c3512d3a2b687f8df674d68f
Manuel Ulate <fr3zs09@gmail.com>
2023-10-27 16:18:39 -0600

    new: hunter x hunter list page

---
+++ src/modules/characters/views/HxHListPage.tsx
@@ -0,0 +1,30 @@
+import { useCharacters } from "..";
+import { Loader } from "../../shared/components";
+import { CharacterCard } from "../components";
+
+
+export const HxHListPage = () => {
+
+    const { isLoading, characters } = useCharacters({ filterKey: "Hunter X Hunter" });
+
+    return (
+        <section className="list-container p-4">
+            <h1 className="text-center text-4xl mt-4 mb-8 dark:text-slate-100">Lista de personajes</h1>
+            {
+                isLoading 
+                ? ( <Loader /> )
+                :
+                <div className="grid grid-cols-1 md:grid-cols-4 md:gap-3 justify-items-center">
+                    {
+                    characters && characters.map(( char ) => (
+                        <CharacterCard 
+                            key={ char.id } 
+                            character={ char }
+                        />
+                    ))
+                    }
+                </div>
+            }
+        </section>
+    )
+}
