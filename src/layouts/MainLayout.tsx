edb905499cdc58aea030f6d569a8755374f14954
Manuel Ulate <fr3zs09@gmail.com>
2023-10-26 22:20:22 -0600

    new: mode button

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import { Sidebar, Footer } from '../modules/shared/components';
+import { Sidebar, Footer, ModeButton } from '../modules/shared/components';
 
 export const MainLayout = ( ) => {
   return (
     <body className=''>
+        <ModeButton />
         <Sidebar />
-            <main
-                className="float-right inline w-[calc(100%-16rem)] h-[calc(100%-77.53px)] dark:bg-gray-800"
-            >
-              <Outlet />
-            </main>
+        <main
+            className="float-right inline w-[calc(100%-16rem)] h-[calc(100%-77.53px)] dark:bg-gray-800"
+        >
+          <Outlet />
+        </main>
         <Footer />
     </body>
   )
--- src/modules/shared/components/ModeButton.tsx
+++ src/modules/shared/components/ModeButton.tsx
@@ -1,29 +1,21 @@
+import { SunIcon } from "@heroicons/react/24/outline"
 
 
 export const ModeButton = () => {
   return (
-    <>
-        <button id="dropdown-button" data-dropdown-toggle="dropdown-menu" data-dropdown-placement="right" className="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shrink-0 focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button"> 
-            <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
-            </svg>
+    <div className="fixed right-0 px-8 py-2">
+        <button 
+            id="dropdown-button" 
+            data-dropdown-toggle="dropdown-menu" 
+            data-dropdown-placement="right" 
+            className="p-3 bg-white border border-gray-200 rounded-full shrink-0 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" 
+            type="button"
+        > 
+            <SunIcon 
+                width={ 18 }
+                color="#fabf0c"
+            />
         </button>
-        <div id="dropdown-menu" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Report this page</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Add to favorites</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Block this page</a>
-            </li>
-            <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Invite users</a>
-            </li>
-            </ul>
-        </div>
-    </>
+    </div>
   )
 }
