450c6b9ceab544c129110e50f94bf92df2ce64b6
Manuel Ulate <fr3zs09@gmail.com>
2023-10-26 22:21:39 -0600

    new: mode button

--- src/modules/shared/components/ModeButton.tsx
+++ src/modules/shared/components/ModeButton.tsx
@@ -8,7 +8,7 @@ export const ModeButton = () => {
             id="dropdown-button" 
             data-dropdown-toggle="dropdown-menu" 
             data-dropdown-placement="right" 
-            className="p-3 bg-white border border-gray-200 rounded-full shrink-0 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" 
+            className="p-3 z-10" 
             type="button"
         > 
             <SunIcon 
