80e6d0ee1f65efe1b0b7f9dfe67d19a3d9808855
Manuel Ulate <fr3zs09@gmail.com>
2023-10-28 16:34:21 -0600

    new: onMutate, success - error

--- src/modules/characters/hooks/useNewCharacter.ts
+++ src/modules/characters/hooks/useNewCharacter.ts
@@ -1,5 +1,7 @@
 import { useForm } from 'react-hook-form';
 import { useQueryClient, useMutation } from '@tanstack/react-query'
+import { toast } from 'sonner';
+import moment from 'moment';
 import { CharacterLike } from '../interfaces/character';
 import { charactersActions } from '..';
 
@@ -22,15 +24,32 @@ export const useNewCharacter = () => {
     const mutation = useMutation({
         mutationFn: charactersActions.newCharacter,
         onSuccess: ( character, _vars ) => {
+            toast.success("Personaje agregado correctamente", {
+                description: `${ character.name } agregado ${ moment().format('MMMM Do YYYY, h:mm:ss a')}`
+            });
             
-        } 
+            queryClient.invalidateQueries({
+                queryKey: ['characters', { filterKey: character.serie }]
+            });
+
+            queryClient.invalidateQueries({
+                queryKey: ['characters', {}]
+            });
+        },
+        onError: ( _error, vars ) => {
+            console.log( _error );
+            toast.error("Ups, ha ocurrido un error al agregar el personaje", {
+                description: `${ vars.name } intento ${ moment().format('MMMM Do YYYY, h:mm:ss a')}`
+            });
+        }
     })
 
 
     return {
         register,
         watch,
-        handleSubmit
-    
+        handleSubmit,
+        mutation, 
+        queryClient
     }
 }
--- src/modules/characters/views/NewCharacter.tsx
+++ src/modules/characters/views/NewCharacter.tsx
@@ -2,6 +2,7 @@ import { ListHeader } from '..';
 
 
 export const NewCharacter = () => {
+
   return (
     <section className="list-container p-4">
         <ListHeader title="Nuevo personaje" hideSearcher/>
