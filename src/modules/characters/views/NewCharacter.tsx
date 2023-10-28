9a54bbfc5b9836735c78e0376848659e4aca84b0
Manuel Ulate <fr3zs09@gmail.com>
2023-10-28 15:21:40 -0600

    new: NewCharacterPage initial

--- src/modules/characters/views/NewCharacter.tsx
+++ src/modules/characters/views/NewCharacter.tsx
@@ -3,6 +3,8 @@ import { ListHeader } from ".."
 
 export const NewCharacter = () => {
   return (
-    <></>
+    <section className="list-container p-4">
+        <ListHeader title="Nuevo personaje" hideSearcher/>
+    </section>
   )
 }
