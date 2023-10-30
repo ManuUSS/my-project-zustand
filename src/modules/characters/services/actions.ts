d84d879c9bb8e33eaf315dc4a7f62356353b0431
Manuel Ulate <fr3zs09@gmail.com>
2023-10-29 21:09:57 -0600

    fix: actions commnets

--- src/modules/characters/services/actions.ts
+++ src/modules/characters/services/actions.ts
@@ -5,22 +5,37 @@ interface GetCharacterOptions {
     id?: number;
 }
 
+/**
+ * Performs a GET Petition to Characters API
+ * @param {{ filterKey }}
+ * @returns { data } CharacterResponse
+ */
 export const getCharacters = async ({ filterKey }:GetCharacterOptions ):Promise<CharacterResponse[]> => {
     
+    // <--- URL Params to filter response --->
     const params = new URLSearchParams();
-    
+
     if( filterKey ) {
         params.append('serie', filterKey );
     }
+
+    // <--- Performs API petition --->
     const { data } = await charactersApi.get<CharacterResponse[]>('/characters', { params });
     await delay( 5000 );
     return data;
 
 }
 
+/**
+ * Performs a POST Petition to Characters API
+ * @param { character } CharacterLike
+ * @returns { data } CharacterResponse
+ */
 export const newCharacter = async ( character: CharacterLike ):Promise<CharacterResponse> => {
 
     await delay( 5000 );
+    
+    // <--- Performs API petition --->
     const { data } = await charactersApi.post<CharacterResponse>('/characters', character );
 
     return data;
