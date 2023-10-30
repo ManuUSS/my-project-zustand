e70266de86e85a125cb149793b2c0055df9b8f2f
Manuel Ulate <fr3zs09@gmail.com>
2023-10-29 21:42:01 -0600

    new: new character real time card

--- src/modules/characters/views/NewCharacter.tsx
+++ src/modules/characters/views/NewCharacter.tsx
@@ -1,11 +1,21 @@
-import { CharacterForm, ListHeader } from '..';
+import { StopIcon } from '@heroicons/react/24/solid';
+import { CharacterForm, ListHeader, Status } from '..';
 import { useNewCharacter } from '../hooks/useNewCharacter';
 
 
 export const NewCharacter = () => {
   
-  const { register, handleSubmit, onNewCharacter, mutation } = useNewCharacter();
+  const { register, handleSubmit, onNewCharacter, mutation, watch } = useNewCharacter();
 
+  const validateStatus = ( status: Status ) => {
+    if( status === "alive" )
+        return "text-green-500"
+
+    else if ( status === "dead" )
+        return "text-red-500"
+
+    return "text-slate-400"
+}
 
   return (
     <section className="list-container p-4">
@@ -17,8 +27,35 @@ export const NewCharacter = () => {
                 onNewCharacter={ onNewCharacter }
                 isPending={ mutation.isPending }
             />
-            <div className="p-3 border rounded-md dark:border-gray-700">
-
+            <div className="border rounded-md dark:border-gray-700">
+                <div className="flex items-center">
+                    {
+                        watch("image") &&
+                        (
+                            <img 
+                                className="object-cover h-80 w-full"
+                                src={ watch("image") } 
+                                alt={ watch("name")} 
+                            />
+                        )
+                    }
+                </div>
+                <div className="p-5">
+                    <div className="flex justify-between items-center mb-2">
+                        <div>
+                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-200">{ watch("name") }</h5>
+                            <p className="dark:text-slate-300">{ watch("serie") }</p>
+                        </div>
+                        {
+                            watch("status") && (
+                                <StopIcon className={`w-5 ${ validateStatus( watch("status") )}`} />
+                            )
+                        }
+                    </div>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-slate-300">
+                        { watch("about") }
+                    </p>
+                </div>
             </div>
         </div>
     </section>
