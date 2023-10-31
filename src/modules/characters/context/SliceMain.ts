0a8fdc057d1574c45fbff71360c997b3893cf5d8
Manuel Ulate <fr3zs09@gmail.com>
2023-10-30 21:59:38 -0600

    new: main slice

---
+++ src/modules/characters/context/SliceMain.ts
@@ -0,0 +1,21 @@
+import { StateCreator } from 'zustand';
+import { CharacterResponse } from '..';
+
+export interface MainSlice {
+    mainList: CharacterResponse[];
+    setMainList: ( list:CharacterResponse[] ) => void;
+    addToMainList: ( char: CharacterResponse ) => void;
+    filterMainList: () => void;
+}
+
+
+export const createJJSlice:StateCreator<MainSlice> = ( set ) => ({
+    mainList: [],
+    setMainList: ( list: CharacterResponse[] ) => {
+        set(() => ({ mainList: list }))
+    },
+    addToMainList: ( char: CharacterResponse ) => {
+        set(( ctx ) => ({ mainList: [ ...ctx.mainList, char ] }))
+    },
+    filterMainList: () => {},
+})
