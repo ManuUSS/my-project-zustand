9df4f42a0d55e37ce182e4d252e35467856567eb
Manuel Ulate <fr3zs09@gmail.com>
2023-10-31 17:22:41 -0600

    fix: useCharacter doc

--- src/modules/characters/hooks/useCharacter.ts
+++ src/modules/characters/hooks/useCharacter.ts
@@ -8,15 +8,21 @@ interface Props {
 
 export const useCharacter = ({ characterId }:Props) => {
 
+    // <--- Main Store | Zustand --->
     const { mainList } = useCharactersStore();
+    // <--- Local State --->
     const [ character, setCharacter ] = useState<CharacterResponse | undefined>( undefined );
     const navigate = useNavigate();
-
+    // <--- Triggers an effect --->
     useEffect(() => {
         getCharactertById();
     }, [])
     
 
+   /**
+    * Retrieves a character by their unique identifier (ID) and sets it as the currently selected character.
+    * If the character with the specified ID is not found, it navigates to the home page.
+    */
     const getCharactertById = () => {
         if( !mainList || !characterId ) return;
         const characterToSet = mainList.find(( char ) => char.id === characterId );
