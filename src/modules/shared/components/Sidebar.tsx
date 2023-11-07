6c340364449b27a46d8f4a708186456c151fd127
Manuel Ulate <fr3zs09@gmail.com>
2023-11-06 22:03:08 -0600

    fix: sidebar header

--- src/modules/shared/components/Sidebar.tsx
+++ src/modules/shared/components/Sidebar.tsx
@@ -20,7 +20,7 @@ export const Sidebar = () => {
             aria-label="Sidebar"
         >   
             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-[#212C3B] ">
-                <h1 className="text-center text-2xl border-b-2 border-b-sky-500 text-sky-600 dark:border-b-sky-200 dark:text-sky-200 pb-2 mb-2 select-none">Guía Zustand</h1>
+                <h1 className="text-center text-2xl border-b-2 border-b-sky-500 text-sky-600 dark:border-b-sky-200 dark:text-sky-200 pb-2 mb-2 select-none">Zest</h1>
                 <ul className="space-y-2 text-lg">
                     <li>
                         <NavLink
