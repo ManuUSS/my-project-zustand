6f12317aaca12f98ea1297f4fa25305322250b8a
Manuel Ulate <fr3zs09@gmail.com>
2023-11-08 22:33:35 -0600

    new: utils docs

--- src/modules/characters/utils/validateStatus.ts
+++ src/modules/characters/utils/validateStatus.ts
@@ -1,5 +1,11 @@
 import { Status } from ".."
 
+/**
+ * Determines the CSS text color class based on the character's status.
+ *
+ * @param {string} status - The status of the character ("alive," "dead," or other).
+ * @returns {string} A CSS class for text color based on the character's status.
+ */
 export const validateStatus = ( status: Status ) => {
     if( status === "alive" )
         return "text-[#06d6a0]"
@@ -10,6 +16,12 @@ export const validateStatus = ( status: Status ) => {
     return "text-slate-400"
 }
 
+/**
+ * Determines the CSS text color class based on the character's status.
+ * Its used on the searcher's icons
+ * @param {string} status - The status of the character ("alive," "dead," or other).
+ * @returns {string} A CSS class for text color based on the character's status.
+ */
 export const validateStautsFilter = ( status: string ):string => {
 
     if( status === "alive" ) return "text-[#06d6a0]";
@@ -19,6 +31,12 @@ export const validateStautsFilter = ( status: string ):string => {
     return "text-sky-500";
 }
 
+/**
+ * Get the text representation of a character's status.
+ *
+ * @param {string} status - The status of the character ("alive," "dead," or other).
+ * @returns {string} The text representation of the character's status ("Vivo," "Muerto," or "Desconocido").
+ */
 export const getStatusText = ( status:Status ) => {
     if( status === "alive" ) return "Vivo"
     else if ( status === "dead" ) return "Muerto"
