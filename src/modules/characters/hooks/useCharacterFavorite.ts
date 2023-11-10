1d2aee2ba42be5b59650fd53eda1903eb80cd0ee
Manuel Ulate <fr3zs09@gmail.com>
2023-11-09 22:00:44 -0600

    new: removeFromFavoriteList applied on hook

--- src/modules/characters/hooks/useCharacterFavorite.ts
+++ src/modules/characters/hooks/useCharacterFavorite.ts
@@ -2,12 +2,13 @@ import { createElement } from 'react';
 import { toast } from 'sonner';
 import moment from 'moment';
 import { CharacterResponse, useFavoriteStore } from '..';
-import { ToasterFavorite } from '../../shared/components';
+import { ToasterFavorite, ToasterInfo } from '../../shared/components';
 
 
 export const useCharacterFavorite = () => {
     
     const addFavoriteState = useFavoriteStore(( state ) => state.addTofavoriteList );
+    const removeFavoriteState = useFavoriteStore(( state ) => state.removeFromFavoriteList );
 
     const onAddFavorite = ( char: CharacterResponse ) => {
         addFavoriteState( char );
@@ -22,7 +23,23 @@ export const useCharacterFavorite = () => {
         ));
     }
 
+    const onRemoveFavorite = ( charName: string ) => {
+        removeFavoriteState( charName );
+        toast.custom(() => (
+            createElement(
+                ToasterInfo,
+                {
+                    message: `${ charName } eliminado de favoritos`,
+                    description: `Personaje eliminado ${ moment().format('MM/DD/YYYY')}`
+                }
+            )
+        ));
+    }
+
+
+
     return {
-        onAddFavorite
+        onAddFavorite,
+        onRemoveFavorite
     }
 }
