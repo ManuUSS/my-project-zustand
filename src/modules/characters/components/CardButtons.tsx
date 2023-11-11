a5ace5010ecb2132636cf4ef34f928eed8939aba
Manuel Ulate <fr3zs09@gmail.com>
2023-11-10 20:41:48 -0600

    new: character card buttons component

---
+++ src/modules/characters/components/CardButtons.tsx
@@ -0,0 +1,42 @@
+import { EllipsisVerticalIcon, StarIcon as StarIconOutline } from '@heroicons/react/24/outline';
+import { StarIcon as StarIconSolid } from '@heroicons/react/24/solid';
+import { CharacterResponse, useCharacterFavorite, useFavoriteStore } from '..';
+import { FC } from 'react';
+
+interface Props {
+    character: CharacterResponse;
+}
+
+export const CardButtons:FC<Props> = ({  character }) => {
+
+    const { hasCharacter } = useFavoriteStore();
+    const { onAddFavorite, onRemoveFavorite } = useCharacterFavorite();
+
+    return (
+        <div
+            className="flex justify-between items-center"
+        >
+            {
+                hasCharacter( character.name ) 
+                ? ( 
+                    <StarIconSolid 
+                        width={ 20 } 
+                        className="cursor-pointer text-[#fabf0c]"
+                        onClick={ () => onRemoveFavorite( character.name )}
+                    /> 
+                )
+                : ( 
+                    <StarIconOutline 
+                        width={ 20 } 
+                        className="cursor-pointer text-gray-400 dark:text-gray-600" 
+                        onClick={ () => onAddFavorite( character ) } 
+                    /> 
+                )
+            }
+            <EllipsisVerticalIcon
+                width={ 20 } 
+                className='text-gray-600 text-center dark:text-slate-300 cursor-pointer'
+            />
+        </div>
+    )
+}
