d04142fc5fce5cacf853d5b1e85b5f9f04fa54a8
Manuel Ulate <fr3zs09@gmail.com>
2023-11-11 21:49:55 -0600

    docs: useCharacter custom hook

--- src/modules/characters/hooks/useCharacter.ts
+++ src/modules/characters/hooks/useCharacter.ts
@@ -6,9 +6,13 @@ interface Props {
 }
 
 /**
- * Perfoms different functionalities
- * @param {{ characterId }} number
- * @returns Object
+ * A custom hook for fetching character data using Tanstack Query.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.characterId - The ID of the character to fetch.
+ * @returns {Object} An object containing character data and loading status.
+ * @property {CharacterResponse | undefined} data - The fetched character data.
+ * @property {boolean} isFetching - A boolean indicating whether the query is currently fetching data.
  */
 export const useCharacter = ({ characterId }:Props) => {
 
