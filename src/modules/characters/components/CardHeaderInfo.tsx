e89ef9c3fae4abd86ce00d187ef958b5be94c440
Manuel Ulate <fr3zs09@gmail.com>
2023-11-15 11:00:27 -0600

    docs: card header info component

--- src/modules/characters/components/CardHeaderInfo.tsx
+++ src/modules/characters/components/CardHeaderInfo.tsx
@@ -1,11 +1,20 @@
-import { FC } from "react";
+import { FC } from 'react';
 
 interface Props {
     charName: string;
     charSerie: string;
 }
 
-export const CardHeaderInfo:FC<Props> = ({ charName, charSerie }) => {
+/**
+ * Renders the header information for a character card, including the character's name and series.
+ *
+ * @component
+ * @param {Props} props - The component properties.
+ * @param {string} props.charName - The name of the character to be displayed in the card header.
+ * @param {string} props.charSerie - The series to which the character belongs, displayed in the card header.
+ * @returns {JSX.Element} The rendered header information for the character card.
+ */
+export const CardHeaderInfo:FC<Props> = ({ charName, charSerie }):JSX.Element => {
     return (
         <div>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-slate-200">{ charName }</h5>
