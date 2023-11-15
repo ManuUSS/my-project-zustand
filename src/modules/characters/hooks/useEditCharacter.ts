bbfa63041aab5312343b0f7d305f90b0743dd486
Manuel Ulate <fr3zs09@gmail.com>
2023-11-14 20:41:45 -0600

    fix: mutation edit

--- src/modules/characters/hooks/useEditCharacter.ts
+++ src/modules/characters/hooks/useEditCharacter.ts
@@ -192,7 +192,7 @@ export const useEditCharacter = ({ defaultValues }:Props ) => {
         setPower(( currentPower ) => ({ ...currentPower, [name]: value }));
     }
 
-    // 
+    
     return {
         register,
         errors,
