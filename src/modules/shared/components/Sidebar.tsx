63c75f0ee83bb9f29e1d15b04c66c3749a8c539b
Manuel Ulate <fr3zs09@gmail.com>
2023-11-16 10:55:18 -0600

    new: sideitem component implemented

--- src/modules/shared/components/Sidebar.tsx
+++ src/modules/shared/components/Sidebar.tsx
@@ -16,7 +16,36 @@ export const Sidebar = () => {
             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-[#212C3B] ">
                 <h1 className="text-center text-2xl border-b-2 border-b-sky-500 text-sky-600 dark:border-b-sky-200 dark:text-sky-200 pb-2 mb-2 select-none">Zest</h1>
                 <ul className="space-y-2 text-lg">
-                    
+                    <SideItem 
+                        url=""
+                        iconComponent={ <UsersIcon className="w-5 text-sky-600 dark:text-sky-300" /> }
+                        text="Lista de personajes"
+                    />
+                    <SideItem 
+                        url={ PATHS["JJK"] }
+                        iconComponent={ <CheckBadgeIcon className="w-5 text-sky-600 dark:text-sky-300" /> }
+                        text="Jujutsu Kaisen"
+                    />
+                    <SideItem 
+                        url={ PATHS["DemonSlayer"] }
+                        iconComponent={ <CheckBadgeIcon className="w-5 text-sky-600 dark:text-sky-300" /> }
+                        text="Demon Slayer"
+                    />
+                    <SideItem 
+                        url={ PATHS["HxH"] }
+                        iconComponent={ <CheckBadgeIcon className="w-5 text-sky-600 dark:text-sky-300" /> }
+                        text="Hunter X Hunter"
+                    />
+                    <SideItem 
+                        url={ PATHS["Favorite"] }
+                        iconComponent={ <StarIcon className="w-5 text-sky-600 dark:text-sky-300" /> }
+                        text="Mis favoritos"
+                    />
+                    <SideItem 
+                        url={ PATHS["New"] }
+                        iconComponent={ <PlusIcon className="w-5 text-sky-600 dark:text-sky-300" /> }
+                        text="Agregar nuevo"
+                    />
                 </ul>
             </div>
         </aside>
