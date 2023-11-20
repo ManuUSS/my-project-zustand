5f76624c2a81cacd8dc01edbd905920c2168dbd6
Manuel Ulate <fr3zs09@gmail.com>
2023-11-19 22:01:32 -0600

    fix: similar card components hover

--- src/modules/characters/components/CharacterRelated.tsx
+++ src/modules/characters/components/CharacterRelated.tsx
@@ -1,5 +1,6 @@
-import { FC, useState } from 'react';
-import { CharacterResponse } from ".."
+import { FC } from 'react';
+import { CharacterResponse } from '..';
+import './CharacterRelated.css';
 
 interface Props {
     character: CharacterResponse;
@@ -7,17 +8,13 @@ interface Props {
 
 export const CharacterRelated:FC<Props> = ({ character }) => {
 
-    const [ classT, setClassT] = useState( "hidden" )
-
     return (
         <div 
-            className={'relative bg-no-repeat bg-cover flex items-center justify-center rounded-md shadow-sm min-w-[350px] min-h-[180px] mb-2 cursor-pointer'}
+            className={'relative bg-no-repeat bg-cover flex items-center justify-center rounded-md shadow-sm min-w-[350px] min-h-[180px] mb-2 cursor-pointer card-character'}
             style={{ backgroundImage: `url(${ character.image })` }}
-            onMouseEnter={ () => setClassT("visible")}
-            onMouseLeave={ () => setClassT("hidden") }
         >
-            <p className={`${ classT } z-50 text-3xl text-white font-bold text-center transition-all duration-300 ease-in-out`}>{ character.name }</p>
-            <div className={`${ classT } absolute bg-neutral-950/80 top-0 right-0 bottom-0 left-0 transition-all duration-150 ease-in-out`}></div>
+            <p className={`z-50 text-3xl text-white font-bold text-center card-character__text`}>{ character.name }</p>
+            <div className={`absolute bg-neutral-950/80 top-0 right-0 bottom-0 left-0 card-character__cover`}></div>
         </div>
     )
 }
