a9ff5de34ead4062880a719b67cbcf542cef3069
Manuel Ulate <fr3zs09@gmail.com>
2023-11-19 21:51:31 -0600

    fix: similar card components hover

--- src/modules/characters/components/CharacterRelated.tsx
+++ src/modules/characters/components/CharacterRelated.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from 'react';
 import { CharacterResponse } from ".."
 
 interface Props {
@@ -6,13 +6,18 @@ interface Props {
 }
 
 export const CharacterRelated:FC<Props> = ({ character }) => {
+
+    const [ classT, setClassT] = useState( "hidden" )
+
     return (
         <div 
-            className={'relative bg-no-repeat bg-cover flex items-center justify-center rounded-md shadow-sm min-w-[350px] min-h-[180px] mb-2'}
+            className={'relative bg-no-repeat bg-cover flex items-center justify-center rounded-md shadow-sm min-w-[350px] min-h-[180px] mb-2 cursor-pointer'}
             style={{ backgroundImage: `url(${ character.image })` }}
+            onMouseEnter={ () => setClassT("visible")}
+            onMouseLeave={ () => setClassT("hidden") }
         >
-            <p className={`z-50 text-3xl text-white font-bold text-center`}>{ character.name }</p>
-            <div className={`absolute bg-neutral-950/80 top-0 right-0 bottom-0 left-0`}></div>
+            <p className={`${ classT } z-50 text-3xl text-white font-bold text-center transition-all duration-300 ease-in-out`}>{ character.name }</p>
+            <div className={`${ classT } absolute bg-neutral-950/80 top-0 right-0 bottom-0 left-0 transition-all duration-150 ease-in-out`}></div>
         </div>
     )
 }
