2a09fd5551f187376eb41f3c32a3f614d04b7daf
Manuel Ulate <fr3zs09@gmail.com>
2023-11-20 19:44:07 -0600

    change: mode buttons icons

--- src/modules/shared/components/ModeButton.tsx
+++ src/modules/shared/components/ModeButton.tsx
@@ -1,5 +1,5 @@
-import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 import { useThemeStore } from '../../../plugins/ThemeProvider';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
 
 
 export const ModeButton = () => {
